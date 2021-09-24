92415f192a7c3a729fc3abe92644dffe70b591ad
Teodor Boyadzhiev <teodor1497@gmail.com>
2021-09-24 23:51:48 +0300

    commit

--- first-app/src/components/Register.js
+++ first-app/src/components/Register.js
@@ -6,18 +6,19 @@ function Register() {
             <h1>
                 Register page
             </h1>
-
             <input style={{ margin: "5px", padding: "5px" }} placeholder="Username..."></input><br></br>
             <input style={{ margin: "5px", padding: "5px" }} placeholder="Password..."></input><br></br>
             <input style={{ margin: "5px", padding: "5px" }} placeholder="Repeat Password..."></input><br></br>
 
-            <button style={{
+            <button style=
+                {{
                 marginTop: "10px",
                 padding: "7px 15px",
                 backgroundColor: "green",
                 borderRadius: "5px",
                 fontSize: "20px"
-            }}>
+                }}
+            >
                 Register
             </button>
         </div>
