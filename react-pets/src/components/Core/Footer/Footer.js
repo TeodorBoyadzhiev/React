05948a6d155fa3a61d85b305eba41832b3a89379
Teodor Boyadzhiev <teodor1497@gmail.com>
2021-10-24 22:06:35 +0300

    add styles

--- react-pets/src/components/Core/Footer/Footer.js
+++ react-pets/src/components/Core/Footer/Footer.js
@@ -3,10 +3,14 @@ import React from 'react'
 export default function Footer() {
     return (
         <>
-            <footer id="site-footer">
+            <footer id="footer">
                 <p>@PetMyPet</p>
             </footer>
             <style jsx>{`
+            #footer {
+                padding:10px;
+                margin-top: 200px;
+            }
                 `}</style>
         </>
 
