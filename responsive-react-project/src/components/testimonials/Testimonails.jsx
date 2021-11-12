446b5a1fb3bb906fe28d8f5fbbe2796a2a65ac63
Teodor Boyadzhiev <teodor1497@gmail.com>
2021-11-12 23:20:29 +0200

    navBar menu

--- responsive-react-project/src/components/testimonials/Testimonails.jsx
+++ responsive-react-project/src/components/testimonials/Testimonails.jsx
@@ -2,7 +2,7 @@ import './testimonials.scss'
 
 export default function Testimonails() {
     return (
-        <div className="testimonials">
+        <div className="testimonials" id="testimonials">
             
         </div>
     )
--- responsive-react-project/src/components/topbar/Topbar.jsx
+++ responsive-react-project/src/components/topbar/Topbar.jsx
@@ -3,7 +3,12 @@ import './topbar.scss'
 export default function Topbar() {
     return (
         <div className="topbar">
-            
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">genius.</a>
+                </div>
+                <div className="right">dasdsa</div>
+            </div>
         </div>
     )
 }
