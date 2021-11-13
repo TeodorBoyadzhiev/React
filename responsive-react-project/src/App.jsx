04aa95962b790289dd07035b780bd97ab5789cd2
Teodor Boyadzhiev <teodor1497@gmail.com>
2021-11-13 23:15:39 +0200

    add dynamic sidebar menu

--- responsive-react-project/src/App.jsx
+++ responsive-react-project/src/App.jsx
@@ -1,22 +1,22 @@
-import {useState} from "react"
+import { useState } from "react";
 
-import Topbar from "./components/topbar/Topbar"
-import Intro from "./components/intro/Intro"
-import Portfolio from "./components/portfolio/Portfolio"
-import Works from "./components/works/Works"
-import Testimonials from "./components/testimonials/Testimonails"
-import Contact from "./components/contact/Contact"
+import Topbar from "./components/topbar/Topbar";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Works from "./components/works/Works";
+import Testimonials from "./components/testimonials/Testimonails";
+import Contact from "./components/contact/Contact";
+import Menu from "./components/menu/Menu";
 
-import "./app.scss"
-
+import "./app.scss";
 
 function App() {
-
   const [menuOpen, setMenuOpen] = useState(false);
-  
+
   return (
     <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
         <Portfolio />
